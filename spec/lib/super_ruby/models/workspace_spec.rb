c155f18a72f2454b8ddf7c750377d62dbe8fca38
Kevin Finn <superkevin@gmail.com>
2022-05-18 11:44:47 -0400

    add a test for a feature I wish existed

--- spec/lib/super_ruby/models/workspace_spec.rb
+++ spec/lib/super_ruby/models/workspace_spec.rb
@@ -243,4 +243,21 @@ RSpec.describe Workspace do
     expect(workspace.result_type).to eq(Types::Integer.instance)
     expect(workspace.result_value).to eq 13
   end
+
+  xit 'allows calling a procedure which defines a method on a type, then calling that method' do
+    workspace.add_source_string <<~SUPER
+      (sequence(
+        (define method_definer
+          (procedure (t) (t define_method my_method () (self + 1)))
+        )
+        (define MyInteger Integer)
+        (method_definer call MyInteger)
+        (let my_integer MyInteger 7)
+        (my_integer my_method)
+      ))
+    SUPER
+    workspace.evaluate!
+    expect(workspace.result_type).to eq(Types::Integer.instance)
+    expect(workspace.result_value).to eq 8
+  end
 end
